46d409b0cfd184775f4576d45aa4ce46ee5726ac
voodoozulu <55753267+voodoozulu@users.noreply.github.com>
2020-08-30 21:50:30 -0400

    Made rotating buttons for UI

---
+++ PaperFusionUnity/Assets/Battle/Skills/SOSkill.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Skill", menuName = "Skills/skillContainer")]
+
+public class SOSkill :ScriptableObject
+{
+    public Sprite sprite;
+    public Skill skill;
+    public string skillname;
+
+    
+}
